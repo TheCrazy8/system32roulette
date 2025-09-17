3c0f8a862a6ffae8a85b1ad3e0446fed8d20431b
TheCrazy8 <bennettbashford@gmail.com>
2025-09-17 09:35:03 -0400

    Add final warning before starting system32 roulette
    Added a final warning prompt about potential irreversible damage before starting the game.

--- sys32roulette.py
+++ sys32roulette.py
@@ -10,7 +10,12 @@ def main():
         print("Exiting...")
         sys.exit()
     else:
-        print("Starting the game...")
+        if input("FINAL WARNING!!! \n RUNNING THIS *WILL* MOST LIKELY DO IRRIVERSABLE DAMAGE TO YOUR DEVICE.  \n ONLY RUN IN A VIRTUAL MACHINE OR DEVICE YOU DO NOT CARE ABOUT!!! \n I understand the risks, and would like to go ahead with the program. (y/n): ").lower() != 'y':
+            print("Exiting...")
+            sys.exit
+        else:
+            pass
+    print("Starting the game...")
     files = os.listdir(system32_path)
     random_file = random.choice(files)
     print(f"Your random system32 file is: {random_file}")
@@ -31,4 +36,5 @@ def main():
 
 if True:
     main()
+
 
